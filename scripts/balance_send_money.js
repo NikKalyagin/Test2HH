86dfe5257b4aadd03cb229dceed2dbdaded3cfa6
Nikita <nik.kalyagin@gmail.com>
2024-01-30 16:16:01 +0500

    1

--- scripts/balance_send_money.js
+++ scripts/balance_send_money.js
@@ -1,14 +1,22 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const provider = ethers.getDefaultProvider();
-
+  //const provider = ethers.getDefaultProvider();
+  const url = "http://127.0.0.1:8545/";
+  const provider = new ethers.JsonRpcProvider(url);
   //let [sender] = await ethers.getSigners();
   let sender = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
   //let signer = provider.getSigner(sender);
-
+  //const balance = await provider.getBalance(address);
   const balance = await provider.getBalance(sender);
-  console.log(balance);
+  console.log(ethers.formatEther(balance));
+  let amount = ethers.parseEther("10.0");
+
+  let receiver = "0x71bE63f3384f5fb98995898A86B02Fb2426c5788";
+  let tx = {
+    to: receiver,
+    value: amount,
+  };
 }
 
 main()
